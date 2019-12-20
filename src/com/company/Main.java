6abad67e8b21ad94a9fdea0df82c6c2ee9b3e6ff
suteekan-12 <59079677+suteekan-12@users.noreply.github.com>
2019-12-20 16:38:38 +0700

    lab1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,47 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // create Student object
+        Student mark = new Student();
+        Student tontan = new Student();
+        Student drem = new Student();
+
+        //input data to object
+        mark.name = "X Mark";
+        mark.id = "001";
+        mark.majoe = " CIS ";
+        tontan.name = "TonTan";
+        tontan.id = "002";
+        tontan.majoe = " CIS ";
+        drem.name = "Drem all night";
+        drem.id = "003";
+        drem.majoe = " CS ";
+
+
+        //get data from object
+        System.out.println("1." + mark.name + "" + mark.majoe);
+        System.out.println("2." + tontan.name + "" + tontan.majoe);
+
+
+        //add student to array
+        //array index             0     1      2
+        Student[] oopStudents = {mark,tontan,drem};
+
+        System.out.println("1."+oopStudents[0].name);
+        System.out.println("2."+oopStudents[1].name);
+
+        find_CIS_Student(oopStudents);
+    }
+    // To find CIS Student
+    public static void find_CIS_Student(Student[] Students){
+        for(int i = 0; i < Students.length; i++){
+            //Check students.major is "CIS"
+            if(Students[i].majoe == "CIS"){
+                System.out.println(i+1+"."+ Students[i].name);
+            }
+
+        }
+
+
     }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Student {
+    //ประกาศตัวแปรของ class
+    public String name;
+    public String id;
+    public String majoe;
+}
