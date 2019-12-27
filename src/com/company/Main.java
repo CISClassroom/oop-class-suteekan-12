b36291c7153405cb3653df1d2602f01ce76fed45
suteekan-12 <59079677+suteekan-12@users.noreply.github.com>
2019-12-27 22:01:36 +0700

    HW_1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,34 +3,38 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    // create Student object
+
         Student mark = new Student();
         Student tontan = new Student();
-        Student drem = new Student();
+        Student dream = new Student();
 
-        //input data to object
         mark.name = "X Mark";
         mark.id = "001";
-        mark.majoe = " CIS ";
-        tontan.name = "TonTan";
+        mark.majoe = "CIS";
+        mark.sex = "MAN";
+        tontan.name = "Tan Ton";
         tontan.id = "002";
-        tontan.majoe = " CIS ";
-        drem.name = "Drem all night";
-        drem.id = "003";
-        drem.majoe = " CS ";
+        tontan.majoe = "CIS";
+        tontan.sex = "MAN";
+        dream.name = "dream or night";
+        dream.id = "003";
+        dream.majoe = "CS";
+        dream.sex = "WOMAN";
 
 
         //get data from object
-        System.out.println("1." + mark.name + "" + mark.majoe);
-        System.out.println("2." + tontan.name + "" + tontan.majoe);
+        System.out.println("1." + mark.name + " " + mark.majoe +" "+ mark.sex);
+        System.out.println("2." + tontan.name + " " + tontan.majoe +" "+ tontan.sex);
+        System.out.println("3." + dream.name + " " + dream.majoe +" "+ dream.sex);
 
 
         //add student to array
         //array index             0     1      2
-        Student[] oopStudents = {mark,tontan,drem};
+        Student[] oopStudents = {mark,tontan,dream};
 
         System.out.println("1."+oopStudents[0].name);
         System.out.println("2."+oopStudents[1].name);
+        System.out.println("2."+oopStudents[2].name);
 
         find_CIS_Student(oopStudents);
     }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,8 +1,8 @@
 package com.company;
 
 public class Student {
-    //ประกาศตัวแปรของ class
     public String name;
     public String id;
     public String majoe;
+    public String sex;
 }
